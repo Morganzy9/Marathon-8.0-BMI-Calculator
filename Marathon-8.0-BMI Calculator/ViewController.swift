a11d8fbca3374a266bb5a0557339dde585ff43f8
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-03 20:51:36 +0500

    Initial Commit

---
+++ Marathon-8.0-BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon-8.0-BMI Calculator
+//
+//  Created by Ислам Пулатов on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
